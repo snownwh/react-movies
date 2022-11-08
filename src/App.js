6645295630dff212242216ff5e288edd430ac470
WooHyun Nam <woohyunnam@WooHyunui-MacBookPro.local>
2022-11-09 01:28:24 +0900

    path 수정2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename="react-movies">
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
